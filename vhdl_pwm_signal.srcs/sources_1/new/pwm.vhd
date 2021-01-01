d8ca4da9362ba4809932137d42ec29cead37d22a
John Bryan Valle <jbvalle.jv@gmail.com>
2021-01-01 23:33:33 +0100

    Update

--- vhdl_pwm_signal.srcs/sources_1/new/pwm.vhd
+++ vhdl_pwm_signal.srcs/sources_1/new/pwm.vhd
@@ -14,45 +14,40 @@ use IEEE.NUMERIC_STD.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity pwm is
---  Port ( );
-    generic(
-        max_val: integer := 100000000;
-        val_bits: integer := 28
-    );
-    
-    port(
-        clk: in std_logic; --clock input
-        pulse: out std_logic
-    );
+	generic(
+		max_val: integer := 1000;
+		val_bits: integer := 10
+	);
+	port(
+		clk: in std_logic;
+		duty_cycle: in std_logic_vector((val_bits -1) downto 0);
+		pulse: out std_logic
+	);
 end pwm;
 
 architecture Verhalten of pwm is
-    signal counter: std_logic_vector((val_bits-1) downto 0); -- same size as max val and dutycycle
-    signal duty_cycle: std_logic_vector((val_bits-1) downto 0); --duty Cycle | duty Cycle 50% = maxval / 2
-    signal state: std_logic := '1';
+	signal cnt: std_logic_vector((val_bits -1) downto 0);
 begin
-
-count:  process(clk) --counting
-            begin
-                if(rising_edge(clk)) then
-                    if(counter < (max_val-1)) then
-                        counter <= counter + 1;
-                    else
-                        counter <= (others => '0');
-                    end if;
-                end if;
-            end process;
-            
-pulsing: process(clk)
-
-            begin
-                if(rising_edge(clk)) then
-                    if(duty_cycle > counter) then
-                        pulse <= '1';
-                    else
-                        pulse <= '0';
-                    end if;
-                end if;
-            end process;
-               
+    
+    process(clk) -- Counting
+    begin
+        if(clk'event and clk = '1') then
+            if (cnt < (max_val-1)) then
+                cnt <= cnt + 1;
+            else
+                cnt <= (others => '0');
+            end if;
+        end if;
+    end process;
+    
+    process(clk) -- Pulsing
+    begin
+        if(clk'event and clk = '1') then
+            if (duty_cycle > cnt) then
+                pulse <= '1';
+            else
+                pulse <= '0';
+            end if;
+        end if;
+    end process;
 end Verhalten;
